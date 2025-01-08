1585dc1cfbfa9d7b773a0fbea51f1bfe517f0c1e
Conner Krenzer <krenzer84@gmail.com>
2025-01-08 09:08:35 -0500

    middleware wired

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,66 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const {
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId,
+} = require("./accounts-middleware");
+const Account = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", checkAccountId, async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    res.json(account);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+router.post(
+  "/",
+  checkAccountPayload,
+  checkAccountNameUnique,
+  (req, res, next) => {
+    try {
+      res.json("post request");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+  (req, res, next) => {
+    try {
+      res.json("put request");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete("/:id", checkAccountId, (req, res, next) => {
+  try {
+    res.json("delete request");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 module.exports = router;
