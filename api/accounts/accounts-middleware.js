21092de4486041f53209a07bd99aafb6aaeb53d5
Conner Krenzer <krenzer84@gmail.com>
2025-01-08 09:37:18 -0500

    'checkAccountNameUnique middleware complete'

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,26 +1,38 @@
 const Account = require("./accounts-model");
+const db = require("../../data/db-config");
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = { status: 400 };
   const { name, budget } = req.body;
   if (name === undefined || budget === undefined) {
     error.message = "name and budget are required";
-    next(error);
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     error.message = "name of account must be between 3 and 100";
-    next(error);
   } else if (typeof budget !== "number") {
     error.message = "budget of account must be a number";
-    next(error);
   } else if (budget < 0 || budget > 1000000) {
     error.message = "budget of account is too large or too small";
+  }
+  if (error.message) {
     next(error);
+  } else {
+    next();
   }
 };
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  console.log("checkAccountNameUnique middleware");
-  next();
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existingName = await db("accounts")
+      .where("name", req.body.name.trim())
+      .first();
+    if (existingName) {
+      next({ status: 400, message: "that name is taken" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.checkAccountId = async (req, res, next) => {
